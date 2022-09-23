a625d06e4ff687b06b0131d0a398c89529b8ab96
Ryan <rwstauffer72@gmail.com>
2022-09-22 20:34:29 -0400

    change organism and finish unit numbers

--- OrganismClass.py
+++ OrganismClass.py
@@ -1,65 +1,89 @@
 
 class Organism:
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
 
 
 class PP (Organism):
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
 
 
 class PC (Organism):  # Primary Consumer
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
 
 
 class SC (Organism):  # Secondary Consumer
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
 
 
 class TC (Organism):  # Tertiary Consumer
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
 
 
 class DP (Organism):
-    def __init__(self, mass_per_organism, num_of_organism, unit_num):
+    def __init__(self, mass_per_organism, organism_per_acre=None, acre=None,  num_of_organism=None, unit_num=None):
         self.mpo = mass_per_organism
-        self.noo = num_of_organism
-        self.biomass = self.mpo * self.noo
-        if unit_num != None:
+        if num_of_organism is not None:
+            self.noo = num_of_organism
+            self.biomass = self.noo * self.mpo
+        if organism_per_acre is not None:
+            self.opa = organism_per_acre
+            self.biomass = self.mpo * acre * self.opa
+        if unit_num is not None:
             self.unit_num = unit_num
         else:
             self.unit_num = num_of_organism
--- PrimaryProducers.py
+++ PrimaryProducers.py
@@ -1,11 +1,13 @@
 from OrganismClass import *
-Bamboo = PP()
-Junipers = PP()
-SoyBeans = PP()
-BottleGourds = PP()
-Hemp = PP()
-Poppy = PP()
-Ginkgo = PP()
-Moss = PP()
-Ferns = PP()
-Rye = PP()
+
+Bamboo = PP(mass_per_organism=16000, organism_per_acre=1000, acre=)
+Junipers = PP(mass_per_organism=92475, organism_per_acre=35, acre=)
+SoyBeans = PP(mass_per_organism=231.2, organism_per_acre=100,000, acre=)
+BottleGourds = PP(mass_per_organism=10000, organism_per_acre=600, acre=)
+Hemp = PP(mass_per_organism=5101, organism_per_acre=3000, acre=)
+Poppy = PP(mass_per_organism=499, organism_per_acre=21700, acre=)
+Ginkgo = PP(mass_per_organism=92475, organism_per_acre=550, acre=)
+Moss = PP(mass_per_organism=1155.9, organism_per_acre=, acre=100)
+Ferns = PP(mass_per_organism=554.8, organism_per_acre=21700, acre=)
+Rye = PP(mass_per_organism=27215.5, organism_per_acre=40, acre=) 
+#organism per acre is bushel per acre, mass per organism is mass per bushel
